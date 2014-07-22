c20db6e51e1b7219cb4ff099be8079f4fa266401
Joshua Kidd <joshua@Joshuas-iMac.local>
2014-07-22 13:27:11 -0700

    restructured application. added debug logs. everything is more decoupled now.

--- app.js
+++ app.js
@@ -1,75 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/bacp');
+var debug = require('debug')('app'),
+    app = require('./lib/server'),
+    db = require('./lib/db');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var setup = require('./routes/setup');
+require('./lib/config')(__dirname, app);
+require('./lib/router')(app);
 
-var app = express();
-
-var passport = require('passport');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res, next) {
-    req.db = db;
-    req.passport = passport;
-    next();
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port %d in %s mode', server.address().port, process.env.NODE_ENV);
 });
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/setup', setup);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;

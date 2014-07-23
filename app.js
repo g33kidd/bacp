cf2312eaf15b962607d3004b5e0ca262fc4d36b0
Joshua Kidd <kidd.josh.343@gmail.com>
2014-07-22 17:59:11 -0700

    Added routes and handlers.

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var debug = require('debug')('app'),
     app = require('./lib/server'),
     db = require('./lib/db');
 
+// Router Handlers
+var basicHandler = require('./lib/handler/index')(db);
+var userHandler = require('./lib/handler/user')(db);
+
 require('./lib/config')(__dirname, app);
-require('./lib/router')(app);
+require('./lib/router')(app, basicHandler);
+require('./lib/router/users')(app, userHandler);
 
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {

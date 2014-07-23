a3567c2cd780d601481ccf728bc119e0b92f1c77
Joshua Kidd <kidd.josh.343@gmail.com>
2014-07-23 01:38:49 -0700

    Added basic setup page and handler

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var debug = require('debug')('app'),
     app = require('./lib/server'),
+    redis = require('./lib/redis'),
     db = require('./lib/db');
 
 // Router Handlers
 var basicHandler = require('./lib/handler/index')(db);
 var userHandler = require('./lib/handler/user')(db);
+var setupHandler = require('./lib/handler/setup')(db);
 
 require('./lib/config')(__dirname, app);
 require('./lib/router')(app, basicHandler);
 require('./lib/router/users')(app, userHandler);
+require('./lib/router/setup')(app, setupHandler);
 
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {

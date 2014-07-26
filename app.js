1b6019f8651c4aa9ce0f9f68cf5c28b8d465bcef
Joshua Kidd <kidd.josh.343@gmail.com>
2014-07-25 22:18:53 -0700

    Added setup routes and check if installed on start
    Automatically routes every request to the setup page if the redis value
    ‘installed’ is false.

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ var models = require('./lib/models');
 var controllers = require('./lib/controllers');
 var routes = require('./lib/routes');
 
-require('./lib/config')(__dirname, app);
 
+// Setup config and routes... change config
+// setup to method instead of this below.
+require('./lib/config')(__dirname, app);
 routes.setup(app, controllers);
 
 app.set('port', process.env.PORT || 3000);
-
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port %d in %s mode', server.address().port, process.env.NODE_ENV);
+
+  redis.get('installed', function(err, val) {
+    if(err) throw(err);
+    if(val == null) {
+      redis.set('installed', false);
+    }
+  });
+
+  redis.get('setup', function(err, val) {
+    if(err) throw(err);
+    if(val == null) {
+      redis.set('setup', false);
+    }
+  });
+
 });
